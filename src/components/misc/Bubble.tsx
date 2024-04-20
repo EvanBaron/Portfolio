ccd9f68707ca7754fb1c789077aad18495d3152a
EvanBaron <evanbaron.a4@gmail.com>
2024-04-20 12:04:26 +0200

    Fixed overlapping texts and more

--- src/components/misc/Bubble.tsx
+++ src/components/misc/Bubble.tsx
@@ -1,21 +1,24 @@
-import { ReactNode, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import "./bubble.css";
 
 interface BubbleProps {
   className: string;
   animationClass: string;
   children: ReactNode;
+  setIsReading: Dispatch<SetStateAction<boolean>>;
 }
 
 const Bubble: React.FC<BubbleProps> = ({
   className,
   animationClass,
   children,
+  setIsReading,
 }) => {
   const [isPopped, setIsPopped] = useState(false);
 
   const popBubble = () => {
     setIsPopped(true);
+    setIsReading(true);
   };
 
   return (
--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Bubble from "../../components/misc/Bubble";
 import "./about.css";
-
 function About() {
   const { t } = useTranslation();
+
+  const [isReading, setIsReading] = useState<boolean>(false);
 
   return (
     <div className="section about">
@@ -15,36 +17,59 @@ function About() {
 
       <span className="about_glow"></span>
 
-      <Bubble className="bubble_1" animationClass="floating_3">
+      <Bubble
+        className="bubble_1"
+        animationClass="floating_3"
+        setIsReading={setIsReading}
+      >
         {t("bubble_1_1")} <br />
-        {t("bubble_1_2")} <br />
-        {t("bubble_1_3")}
+        {t("bubble_1_2")}
       </Bubble>
 
-      <Bubble className="bubble_2" animationClass="floating_4">
+      <Bubble
+        className="bubble_2"
+        animationClass="floating_4"
+        setIsReading={setIsReading}
+      >
         {t("bubble_2_1")}
       </Bubble>
 
-      <Bubble className="bubble_3" animationClass="floating_2">
+      <Bubble
+        className="bubble_3"
+        animationClass="floating_2"
+        setIsReading={setIsReading}
+      >
         {t("bubble_3_1")} <br />
         {t("bubble_3_2")}
       </Bubble>
 
-      <Bubble className="bubble_4" animationClass="floating_3">
+      <Bubble
+        className="bubble_4"
+        animationClass="floating_3"
+        setIsReading={setIsReading}
+      >
         {t("bubble_4_1")} <br />
         {t("bubble_4_2")}
       </Bubble>
 
-      <Bubble className="bubble_5" animationClass="floating_1">
+      <Bubble
+        className="bubble_5"
+        animationClass="floating_1"
+        setIsReading={setIsReading}
+      >
         {t("bubble_5_1")} <br />
         {t("bubble_5_2")}
       </Bubble>
 
-      <Bubble className="bubble_6" animationClass="floating_2">
+      <Bubble
+        className="bubble_6"
+        animationClass="floating_2"
+        setIsReading={setIsReading}
+      >
         {t("bubble_6_1")}
       </Bubble>
 
-      <p className="info">{t("info")}</p>
+      <p className={`info ${isReading ? "tracking-out" : ""}`}>{t("info")}</p>
     </div>
   );
 }
